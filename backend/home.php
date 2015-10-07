c18f2de12631623fcc195b88580f08ff9c88dd6c
VadimGhost <salminvadim@gmail.com>
2015-10-07 19:56:08 +0300

    fix src script

--- backend/home.php
+++ backend/home.php
@@ -7,7 +7,7 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <script src="jquery-1.11.3.min.js"></script>
+        <script src="js/jquery-1.11.3.min.js"></script>
     </head>
     <body>
         <input type="text" name="text" id="text">
