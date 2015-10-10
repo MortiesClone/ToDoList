473a9a8a5add083cd0796a894b596f5dd28ed236
VadimGhost <salminvadim@gmail.com>
2015-10-10 09:30:02 +0300

    fix problems

--- backend/home.php
+++ backend/home.php
@@ -8,8 +8,8 @@
 <html>
     <head>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="backend/js/script.min.js"></script>
-        <link rel="stylesheet" href="backend/css/style.min.css">
+        <script src="js/script.min.js"></script>
+        <link rel="stylesheet" href="css/style.min.css">
     </head>
     <body>
         <input type="text" name="text" id="text">
--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,3 @@
 <?php 
-    include 'backend/home.php';
+    include 'home.php';
 ?>
