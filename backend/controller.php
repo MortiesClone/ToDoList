66c0b482a251af5009b3882ce57c556c81171ef2
VadimGhost <salminvadim@gmail.com>
2015-10-17 12:47:23 +0300

    исправлена ajax ошибка

--- backend/controller.php
+++ backend/controller.php
@@ -2,13 +2,14 @@
     require_once 'view.php';
     require_once 'model.php';
 
-    $view = new View();
-    $view->generate('views/base_view.php','views/list_view.php');
-    
     if(isset($_GET['text'])){
         $model = new Model();
         $text = $_GET['text'];
         $id = $model->set_data($text, "0", "NULL");
         echo json_encode(array("id" => $id, "text" => $text));
+    }
+    else{
+        $view = new View();
+        $view->generate('views/base_view.php','views/list_view.php');
     }
 ?>
