552a69a44143d874fc5299da7670444f7d50a6aa
VadimGhost <salminvadim@gmail.com>
2015-10-18 15:41:20 +0300

    fix style

--- backend/views/base_view.php
+++ backend/views/base_view.php
@@ -4,12 +4,24 @@
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="js/script.min.js"></script>
         <link rel="stylesheet" href="css/style.min.css">
+        <style>
+            #inputs{
+                width: 175px;
+                margin-left: 20px;
+            }
+            .sub-item{
+                width: 160px;
+                margin-top: 5px;
+            }
+        </style>
     </head>
     <body>
         <?php include $view; ?>
         <script>
+            var count = 0;
             function add_textfield(){
-                $("#inputs").append("<input class=\"sub-item\" type=\"text\">");
+                $("#inputs").append("<input id=\"id" + count + "\" class=\"sub-item text\" type=\"text\">");
+                count++;
             }
         </script>
     </body>
--- backend/views/list_view.php
+++ backend/views/list_view.php
@@ -1,4 +1,4 @@
-<input type="text" name="text" id="text">
+<input type="text" name="text" class="text">
 <input type="button" class="btn" onclick="add_textfield();" value="Добавить подзадачу">
 <input type="button" class="btn" onclick="send();" value="Готово!">
 <div id="inputs"></div>
