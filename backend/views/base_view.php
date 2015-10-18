12a8d98388b2e14f16954500bbf56c57a4ba9385
VadimGhost <salminvadim@gmail.com>
2015-10-18 19:43:04 +0300

    Добавлено меню сохранения

--- backend/views/base_view.php
+++ backend/views/base_view.php
@@ -6,6 +6,10 @@
         <link rel="stylesheet" href="css/style.min.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <style>
+            body{
+                width: 100%;
+                height: 100%;
+            }
             #inputs{
                 width: 175px;
                 margin-left: 20px;
@@ -13,9 +17,6 @@
             .sub-item{
                 width: 160px;
                 margin-top: 5px;
-            }
-            body{
-                margin: 8px;
             }
             .glyphicon{
                 float: right;
@@ -28,6 +29,29 @@
                     border-bottom: 1px solid #B2BDCB;
                     margin-top: 10px;
             }
+            .window{
+                position: absolute;
+                right: 45%;
+                top: 80px;
+                border: 1px solid #B2BDCB;
+                padding: 25px;
+                border-radius: 3px;
+                background-color: white;
+            }
+            .all{
+                margin: 8px;
+            }
+            #message{
+                position: fixed;
+                width: 100%;
+                height: 100%;
+            }
+            .shadow{
+                background-color: black;
+                opacity: 0.7;
+                width: 100%;
+                height: 100%;
+            }
         </style>
     </head>
     <body>
@@ -38,9 +62,18 @@
                 $("#inputs").append("<input id=\"id" + count + "\" class=\"sub-item text\" type=\"text\">");
                 count++;
             }
+            var activated = false;
             $(".glyphicon-pencil").click(function(){
-                var text = $(this).parent("p").text();
-                $(this).parent("p").text("");
+                if(!activated){
+                    var span = $(this).parent().children().first();
+                    var text = span.text();
+                    span.html("<input type=\"text\" value=\"" + text + "\">");
+                    activated = true;
+                }
+                else
+                {
+                    $("#message").css("display", "block");
+                }
             });
         </script>
     </body>
--- backend/views/list_view.php
+++ backend/views/list_view.php
@@ -1,14 +1,24 @@
-<input type="text" id="text" class="text">
-<input type="button" class="btn" onclick="add_textfield();" value="Добавить подзадачу">
-<input type="button" class="btn" onclick="send();" value="Готово!">
-<div id="inputs"></div>
-<div id="content">
-    <?php
-        if($data == null)
-            echo '<p>список пуст</p>';
-        else
-            foreach($data as $key => $value){
-                echo '<div class="task" data-id="'.$key.'"><span>'.$value.'</span><span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-pencil"></span></div>';
-            }
-    ?>
+<div id="message" style="display: none;">
+    <div class="window">
+        <p>Сохранить?</p>
+        <input type="button" class="btn" value="Сохранить">
+        <input type="button" class="btn" value="Не сохронять">
+    </div>
+    <div class="shadow"></div>
+</div>
+<div class="all">
+    <input type="text" id="text" class="text">
+    <input type="button" class="btn" onclick="add_textfield();" value="Добавить подзадачу">
+    <input type="button" class="btn" onclick="send();" value="Готово!">
+    <div id="inputs"></div>
+    <div id="content">
+        <?php
+            if($data == null)
+                echo '<p>список пуст</p>';
+            else
+                foreach($data as $key => $value){
+                    echo '<div class="task" data-id="'.$key.'"><span>'.$value.'</span><span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-pencil"></span></div>';
+                }
+        ?>
+    </div>
 </div>
